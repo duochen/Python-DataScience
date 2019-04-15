660ed2c36ffbb6b7f333389d2b35a02e210f7b11
duochen <duochen@yahoo.com>
2019-04-14 21:37:20 -0500

    check in code

--- Lecture01/Exercises/scatter_squares.py
+++ Lecture01/Exercises/scatter_squares.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 x_values = list(range(1,1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.get_cmap("Blues"), edgecolor='none', s=40)
 
 # Set chart title and label axes.
 plt.title("Square Numbers", fontsize=24)
@@ -16,5 +17,5 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 # Set the range for each axis.
 plt.axis([0, 1100, 0, 1100000])
 
-#plt.show()
-plt.savefig('squares_plot.png', bbox_inches='tight')
+plt.show()
+#plt.savefig('squares_plot.png', bbox_inches='tight')
