89dc6f63f3087a04d7038ed142d9aa8a867967fa
Duo Chen <dchen@slb.com>
2020-02-13 11:30:36 -0600

    check in code

---
+++ Exercises/Python/Exercise_01.py
@@ -0,0 +1,20 @@
+import urllib.request, re
+from collections import Counter
+
+# Talk to the user and the Internet
+url = input("Enter the URL: ")
+
+try:
+    page = urllib.request.urlopen(url)
+except:
+    print("Cannot open %s" % url)
+    quit()
+
+# Read and partially normalize the page
+doc = page.read().decode('utf-8').lower()
+
+# Split the text into words
+words = re.findall(r"\w+", doc)
+
+# Build a counter and report the answer
+print(Counter(words).most_common(10))
